bcba58d521b76c8f0db2ddf84324d37a320568a4
Abhishek Gupta <110448627+GhostRider987@users.noreply.github.com>
2022-12-08 10:54:19 +0530

    initial commit

---
+++ Day3IntroToConditionalStatements.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    if(n%2==0){
+        if(n>=2 && n<=5){
+            cout<<"Not Weird";
+        }
+        else if(n>=6 && n<=20){
+            cout<<"Weird";
+        }
+        else if(n>20){
+            cout<<"Not Weird";
+        }
+    }
+    else{
+        cout<<"Weird";
+    }
+    return 0;
+}
